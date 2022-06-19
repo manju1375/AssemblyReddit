e80930124fc90e8f16fa3ff8aa98ff2c96ac1269
Manju <manju1375@gmail.com>
2022-06-19 13:36:05 +0530

    Adapter implementation

---
+++ app/src/main/java/com/assembly/task/ui/main/adapter/ImageItemViewHolder.kt
@@ -0,0 +1,9 @@
+package com.assembly.task.ui.main.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_image.view.*
+
+class ImageItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val imageView = itemView.imageView
+}
---
+++ app/src/main/java/com/assembly/task/ui/main/adapter/ImageListAdapter.kt
@@ -0,0 +1,62 @@
+package com.assembly.task.ui.main.adapter
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.assembly.task.R
+import com.assemblytask.models.Children
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import javax.inject.Inject
+
+
+class ImageListAdapter @Inject constructor() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    var dataList = mutableListOf<Children>()
+    private lateinit var itemClickListener:OnImageItemClickListener
+
+    private val requestOptions: RequestOptions = RequestOptions()
+        .centerCrop()
+        .placeholder(R.drawable.loading_drawable)
+        .error(R.drawable.ic_launcher_foreground)
+        .diskCacheStrategy(DiskCacheStrategy.ALL)
+        .priority(Priority.HIGH)
+
+    fun setOnItemClickListener(itemClickListener: OnImageItemClickListener){
+        this.itemClickListener = itemClickListener
+    }
+
+    fun setDetails(list: List<Children>) {
+        this.dataList = list.toMutableList()
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+        return ImageItemViewHolder(view) //object of ImagePostViewHolder will return
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+        val postData = dataList[position]
+        val viewHolder = holder as ImageItemViewHolder
+        //Glide.with(holder.itemView.context).load(postData.data.children.).apply(requestOptions).into(viewHolder.imageView)
+        viewHolder.itemView.setOnClickListener {
+            itemClickListener.onClick(position)
+        }
+    }
+
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    interface OnImageItemClickListener{
+        fun onClick(position: Int)
+    }
+
+}
